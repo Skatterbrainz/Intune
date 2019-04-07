87f9c2f30803d326e8419f75d76a87792b2a1f95
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-04-07 11:22:10 -0400

    AD scan added

--- Get-PwnedUsers.ps1
+++ Get-PwnedUsers.ps1
@@ -1,19 +1,57 @@
-#requires -module HaveIBeenPwned
-#requires -module MSOnline
+#requires -module HaveIBeenPwned,MSOnline
 
-Connect-MsolService -Credential (Get-Credential)
-
-$pcount = 0
-$users = Get-MsolUser
-$users | Foreach-Object {
-	if ((Get-PwnedAccount -EmailAddress $_.UserPrincipalName).Count -gt 0) {
-		Write-Warning "PWNED: $($_.UserPrincipalName) !!"
-		$pcount++
-	}
-}
-if ($pcount -gt 0) {
-	Write-Host "uh oh! $pcount users have been pwned!" -ForegroundColor Red
-}
-else {
-	Write-Host "congratulations! No users have been pwned!" -ForegroundColor Green
+function Get-PwnedUsers {
+    param (
+        [parameter(Mandatory=$False, HelpMessage="AzureAD or Active Directory")]
+        [ValidateSet('AD','AzureAD')]
+        [string] $Target = 'AD'
+    )
+    $pcount = 0
+    $tcount = 0
+    try {
+        switch ($Target) {
+            'AD' {
+                $pageSize = 2000
+                $as = [adsisearcher]"(objectCategory=User)"
+                [void]$as.PropertiesToLoad.Add('cn')
+                [void]$as.PropertiesToLoad.Add('sAMAccountName')
+                [void]$as.PropertiesToLoad.Add('mail')
+                [void]$as.PropertiesToLoad.Add('distinguishedName')
+                $as.PageSize = $pageSize
+                $results = $as.FindAll()
+                foreach ($user in $results) {
+                    $tcount++
+                    $email = $($user.Properties.item('mail') | Out-String).Trim()
+                    if (![string]::IsNullOrEmpty($email)) {
+                        if ((Get-PwnedAccount -EmailAddress $email -ErrorAction SilentlyContinue).Count -gt 0) {
+                            Write-Warning "PWNED: $email !!"
+                            $pcount++
+                        }
+                    }
+                }
+                break;
+            }
+            'AzureAD' {
+                Connect-MsolService -Credential (Get-Credential)
+                $users = Get-MsolUser
+                $users | Foreach-Object {
+                    $tcount++
+	                if ((Get-PwnedAccount -EmailAddress $_.UserPrincipalName).Count -gt 0) {
+		                Write-Warning "PWNED: $($_.UserPrincipalName) !!"
+		                $pcount++
+	                }
+                }
+                break;
+            }
+        }
+        if ($pcount -gt 0) {
+	        Write-Host "Uh oh! $pcount of $tcount users have been pwned!" -ForegroundColor Red
+        }
+        else {
+	        Write-Host "Congratulations! No users have been pwned. (total scanned: $tcount)" -ForegroundColor Green
+        }
+    }
+    catch {
+        Write-Error $Error[0].Exception.Message 
+    }
 }
