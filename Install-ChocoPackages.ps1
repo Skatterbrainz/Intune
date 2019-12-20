d15bd22dabf6152d542417f401aa83642b5393b5
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-12-19 22:41:24 -0500

    Update Install-ChocoPackages.ps1

--- Install-ChocoPackages.ps1
+++ Install-ChocoPackages.ps1
@@ -1,18 +1,12 @@
-$pkglist = @("7zip","adobereader","git","notepadplusplus","paint.net","vscode","vscode-powershell","vscode-icons","vlc","sysinternals","keepass")
-$ChocoInstall = Join-Path ([System.Environment]::GetFolderPath("CommonApplicationData")) "Chocolatey\bin\choco.exe"
+$pkglist = @("7zip","git","github-desktop","notepadplusplus","paint.net","vscode","vscode-powershell","vscode-icons","microsoft-edge-insider-dev")
+$ChocoInstall = "$env:ProgramData\Chocolatey\bin\choco.exe"
 if (!(Test-Path $ChocoInstall)) {
-	try {
-		Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
-	}
-	catch {
-		Throw "Failed to install Chocolatey"
-	}       
+	Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
 }
-
 try {
 	$pkglist | Foreach-Object { 
 		Write-Output "installing: $_"
-		cup $_ -y 
+		cinst $_ -y 
 	}
 }
 catch {
